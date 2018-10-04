ab3887ec3163aa9ce39ce17292baec4adbc2301c
Matthew J Weiss <mxw5234@psu.edu>
2018-10-04 02:05:08 -0400

    Separating the game engine from its user interface.

--- MineSweeper.java
+++ MineSweeper.java
@@ -1,143 +1,31 @@
+package minesweeper;
+
 import java.util.Scanner;
 import java.util.Random;
 
-class MineSweeper {
-    public static void main(String[] args) {
-        MineSweeper game = new MineSweeper();
-        game.play();
+/**
+ * @author Matt Weiss <mattjweiss1@gmail.com>
+ */
+
+public class MineSweeper {
+
+    public int boardSize;
+
+    private Board board;
+
+    MineSweeper(int numMines, int size) {
+        this.board = new Board(numMines, size);
+        this.boardSize = size;
     }
 
-    void play() {
-
-        // Initialize game
-        CommandLineInterface comm = new CommandLineInterface();
-        Settings settings = comm.settingsPrompt();
-        Board board = new Board(settings);
-
-        // Play turns until the user wins or loses
-        boolean win = false;
-        boolean lose = false;
-        boolean over = false;
-        while(!win && !lose) {
-    
-            // Display board
-            printBoard(board);
-    
-            // Get command
-            String[] command = comm.commandPrompt();
-            Action action = ActionFactory.getAction(command);
-    
-            // Apply command
-            action.apply(board);
-            over = action.checkOverConditions(board);
-            lose = action.checkLoseConditions(board);
-            win = over && !lose;
-        }
-    
-        if(win) {
-            System.out.println();
-            System.out.println("You win!");
-            printSolution(board);
-            System.out.println();
-        }
-        else if(lose) {
-            System.out.println();
-            System.out.println("You lose!");
-            printSolution(board);
-            System.out.println();
-        }
-        else {
-            System.out.println();
-            System.out.println("An error occurred.");
-            System.out.println();
-        }
-    }
-   
-    void printBoard(Board board) {
-        System.out.println();
-        System.out.print("   ");
-        for(int i=0; i<board.size; i++) {
-            System.out.print(i);
-            System.out.print(' ');
-        }
-        System.out.println();
-        System.out.println();
-        char[][] boardArray = board.state;
-        int size = boardArray.length; 
-        for(int i=0; i<size; i++){
-            System.out.print(i);
-            System.out.print("  ");
-            for(int j=0; j<size; j++){
-                System.out.print(boardArray[i][j]);
-                System.out.print(' ');
-            }
-            System.out.println();
-        }
-        System.out.println();
+    /** Returns the value of the game board at location (x, y). */
+    public char getBoardValue(int x, int y) {
+        return this.board.state[x][y];
     }
 
-    void printSolution(Board board) {
-        System.out.println();
-        System.out.print("   ");
-        for(int i=0; i<board.size; i++) {
-            System.out.print(i);
-            System.out.print(' ');
-        }
-        System.out.println();
-        System.out.println();
-        char[][] boardArray = board.solution;
-        int size = boardArray.length; 
-        for(int i=0; i<size; i++){
-            System.out.print(i);
-            System.out.print("  ");
-            for(int j=0; j<size; j++){
-                System.out.print(boardArray[i][j]);
-                System.out.print(' ');
-            }
-            System.out.println();
-        }
-        System.out.println();
-    }
-}
-
-class ActionFactory{
-    static Action getAction(String[] command) {
-        String commandName = command[0];
-        int x;
-        int y;
-        switch(commandName) {
-            case "guess" :
-                x = Integer.parseInt(command[1]);
-                y = Integer.parseInt(command[2]);
-                return new Guess(x, y);
-            case "flag" :
-                x = Integer.parseInt(command[1]);
-                y = Integer.parseInt(command[2]);
-                return new Flag(x, y);
-            default :
-                System.out.println("Command not recognized.");
-                return new BadAction();
-        }
-    }
-}
-
-interface Action {
-    void apply(Board board);
-    boolean checkOverConditions(Board board);
-    boolean checkLoseConditions(Board board);
-}
-
-class Guess implements Action {
-    int x;
-    int y;
-
-    Guess(int x, int y) {
-        this.x = x;
-        this.y = y;
-    }
-
-    public void apply(Board board) {
-        if(board.solution[x][y]=='b') { // don't use board.solution.
+    /** Guess that location (x, y) is NOT a mine. */
+    public void guess(int x, int y) {
+        if(board.solution[x][y]=='b') { // TODO replace with OO code
             board.state[x][y] = 'b';
         }
         else {
@@ -145,7 +33,52 @@ class Guess implements Action {
         }
     }
 
-    public boolean checkOverConditions(Board board) {
+    /** Place a flag symbol on location (x, y). */
+    public void flag(int x, int y) {
+        if(board.state[x][y] == 'f') {
+            board.state[x][y] = 'x';
+        }
+        if(board.state[x][y] == 'x') {
+            board.state[x][y] = 'f';
+        }
+        else {
+            // TODO MineSweeper.flag shouldn't print anything.
+            System.out.println("Can only flag x spaces.");
+        }
+    }
+
+    /** Check if the user has won. */
+    public boolean userWon() {
+        if(isOver() && !userLost()) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+    /** Check if the user has lost. */
+    public boolean userLost() {
+        boolean trippedMine = false;
+        for(int i=0; i<board.size; i++) {
+            for(int j=0; j<board.size; j++) {
+                if(board.state[i][j]=='b'){
+                    trippedMine = true;
+                }
+            }
+        }
+        return trippedMine;
+    }
+
+    public void displaySolution() {
+        for(int i=0; i<board.size; i++) {
+            for(int j=0; j<board.size; j++) {
+                board.state[i][j] = board.solution[i][j];
+            }
+        }
+    }
+
+    private boolean isOver() {
         int numSpacesLeft = 0;
         for(int i=0; i<board.size; i++) {
             for(int j=0; j<board.size; j++) {
@@ -162,17 +95,7 @@ class Guess implements Action {
         }
     }
 
-
-    public boolean checkLoseConditions(Board board) {
-        if(board.solution[x][y] == 'b') {
-            return true;
-        }
-        else {
-            return false;
-        }
-    }
-
-    void sweepClear(Board board, int x, int y) {
+    private void sweepClear(Board board, int x, int y) {
         // if there are, count them and mark (x,y) with number
         // if there aren't, clear the space and sweepClear adjacent spaces
         
@@ -204,122 +127,43 @@ class Guess implements Action {
             board.state[x][y] = (char)(numBombs+48);
         }
     }
-}
 
-class Flag implements Action {
-    int x;
-    int y;
-
-    Flag(int x, int y) {
-        this.x = x;
-        this.y = y;
-    }
-
-    public void apply(Board board) {
-        if(board.state[x][y] == 'f') {
-            board.state[x][y] = 'x';
-        }
-        if(board.state[x][y] == 'x') {
-            board.state[x][y] = 'f';
-        }
-        else {
-            System.out.println("Can only flag x spaces.");
-        }
-    }
-
-    public boolean checkOverConditions(Board board) {
-        return false;
-    }
-
-
-    public boolean checkLoseConditions(Board board) {
-        return false;
-    }
-}
-
-class BadAction implements Action { // Don't like this
-    public void apply(Board board) {}
-    public boolean checkOverConditions(Board board) {
-        return false;
-    }
-    public boolean checkLoseConditions(Board board) {
-        return false;
-    }
-}
-
-
-
-class CommandLineInterface {
-    Scanner inputScanner;
-
-    CommandLineInterface() {
-        inputScanner = new Scanner(System.in).useDelimiter("\n");
-    }
-
-    Settings settingsPrompt() {
-        System.out.println("Enter a difficulty level (number of mines).");
-        String input = inputScanner.next();
-        int numMines = Integer.parseInt(input);
-        System.out.println("Enter a size for the board.");
-        input = inputScanner.next();
-        int size = Integer.parseInt(input);
-
-        Settings settings = new Settings(numMines, size);
-        return settings;
-    }
-
-    String[] commandPrompt() {
-        System.out.println("Enter a command.");
-        String input = inputScanner.next();
-        String[] strs = input.split(" ");
-        return strs;
-    }
-
-}
-
-class Settings {
-    int numMines;
-    int size;
-
-    Settings(int numMines, int size) {
-        this.numMines = numMines;
-        this.size = size;
-    }
-}
-
-class Board {
-    char[][] state;
-    char[][] solution;
-    int size;
-    int numMines;
-
-    Board(Settings settings) {
-        size = settings.size;
-        numMines = settings.numMines;
-
-        // create an initial board of 'x's of size `size`
-        state = new char[size][size];
-        for(int i=0; i<size; i++){
-            for(int j=0; j<size; j++){
-                state[i][j] = 'x';
+    private class Board {
+        char[][] state;
+        char[][] solution;
+        int size;
+        int numMines;
+    
+        Board(int numMines, int size) {
+            this.size = size;
+            this.numMines = numMines;
+    
+            // create an initial board of 'x's of size `size`
+            state = new char[size][size];
+            for(int i=0; i<size; i++){
+                for(int j=0; j<size; j++){
+                    state[i][j] = 'x';
+                }
+            }
+    
+            // randomly generate a solution (mine configuration)
+            solution = new char[size][size];
+            for(int i=0; i<size; i++){
+                for(int j=0; j<size; j++){
+                    solution[i][j] = ' ';
+                }
+            }
+            Random random = new Random();
+            int x;
+            int y;
+            for(int z=0; z<numMines; z++){
+                x = random.nextInt(size);
+                y = random.nextInt(size);
+                solution[x][y] = 'b';
             }
         }
-
-        // randomly generate a solution (mine configuration)
-        solution = new char[size][size];
-        for(int i=0; i<size; i++){
-            for(int j=0; j<size; j++){
-                solution[i][j] = ' ';
-            }
-        }
-        Random random = new Random();
-        int x;
-        int y;
-        for(int z=0; z<numMines; z++){
-            x = random.nextInt(size);
-            y = random.nextInt(size);
-            solution[x][y] = 'b';
-        }
     }
+
 }
- 
+
+
---
+++ MineSweeperCLI.java
@@ -0,0 +1,116 @@
+package minesweeper;
+
+import java.util.Scanner;
+import java.util.Random;
+
+public class MineSweeperCLI {
+
+    public static void main(String[] args) {
+        MineSweeperCLI mscli = new MineSweeperCLI();
+        mscli.play();
+    }
+
+    private void play() {
+
+        // Initialize game
+        CommandLineInterface comm = new CommandLineInterface();
+        Settings settings = comm.settingsPrompt();
+        MineSweeper game = new MineSweeper(settings.numMines, settings.size);
+
+        int x;
+        int y;
+
+        // Play turns until the user wins or loses
+        while(!game.userWon() && !game.userLost()) {
+
+            // Display board
+            comm.printBoard(game);
+
+            // Get command
+            String[] command = comm.commandPrompt();
+            x = Integer.parseInt(command[1]);
+            y = Integer.parseInt(command[2]);
+    
+            // Apply command
+            // TODO replace with OO code
+            if(command[0].equals("guess")) {
+                game.guess(x, y);
+            }
+            else if(command[0].equals("flag")) {
+                game.flag(x, y);
+            }
+            else {
+                System.out.println("Invalid command.");
+            }
+        }
+        if(game.userWon()) {
+            //comm.printWinMessage();
+            System.out.println("You win!");
+        }
+        else if(game.userLost()) {
+            //comm.printLoseMessage();
+            System.out.println("You lose!");
+        }
+        game.displaySolution();
+        comm.printBoard(game);
+    }
+
+    private class CommandLineInterface {
+        Scanner inputScanner;
+        CommandLineInterface() {
+            inputScanner = new Scanner(System.in).useDelimiter("\n");
+        }
+        Settings settingsPrompt() {
+            System.out.println("Enter a difficulty level (number of mines).");
+            String input = inputScanner.next();
+            int numMines = Integer.parseInt(input);
+            System.out.println("Enter a size for the board.");
+            input = inputScanner.next();
+            int size = Integer.parseInt(input);
+    
+            Settings settings = new Settings(numMines, size);
+            return settings;
+        }
+        String[] commandPrompt() {
+            System.out.println("Enter a command.");
+            String input = inputScanner.next();
+            String[] strs = input.split(" ");
+            return strs;
+        }
+
+        void printBoard(MineSweeper game) {
+            System.out.println();
+            System.out.print("   ");
+            for(int i=0; i<game.boardSize; i++) {
+                System.out.print(i);
+                System.out.print(' ');
+            }
+            System.out.println();
+            System.out.println();
+            for(int i=0; i<game.boardSize; i++){
+                System.out.print(i);
+                System.out.print("  ");
+                for(int j=0; j<game.boardSize; j++){
+                    System.out.print(game.getBoardValue(i,j));
+                    System.out.print(' ');
+                }
+                System.out.println();
+            }
+            System.out.println();
+        }
+    }
+
+    class Settings {
+        int numMines;
+        int size;
+    
+        Settings(int numMines, int size) {
+            this.numMines = numMines;
+            this.size = size;
+        }
+    }
+
+}
+
+
+
